b732903e57e72ab11567b8ffab4db966425dddb6
bunnycast <berzzubunny@gmail.com>
2022-03-09 21:17:09 +0900

    4. login, logout, signin > login

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,17 @@
 from django.contrib import messages
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.views import LoginView, LogoutView, logout_then_login
 from django.shortcuts import render, redirect
 
 from accounts.forms import SignUpForm
+
+
+login = LoginView.as_view(template_name="accounts/login_form.html")
+
+
+def logout(request):
+    messages.success(request, "Logout Success. Good Bye!")
+    return logout_then_login(request)
 
 
 def signup(request):
@@ -9,8 +19,9 @@ def signup(request):
         form = SignUpForm(request.POST)
         if form.is_valid():
             signed_user = form.save()
+            auth_login(request, signed_user)
             messages.success(request, "Welcome to Sign Up! :D")
-            signed_user.send_welcome_email()    # celery로 비동기 처
+            # signed_user.send_welcome_email()    # celery로 비동기 처
             next_url = request.GET.get('next', '/')
             return redirect(next_url)
     else:
@@ -19,3 +30,4 @@ def signup(request):
     return render(request, 'accounts/signup_form.html', {
         'form': form,
     })
+
